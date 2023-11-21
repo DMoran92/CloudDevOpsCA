4e39cbb279558e9f815f2cb3f2af6a7436a237fa
Damian Moraniec <d.moraniec1@universityofgalway.ie>
2023-11-21 15:23:17 +0000

    initial project setup

---
+++ src/main/java/com/CA/DMoraniecCA/DMoraniecCaApplication.java
@@ -0,0 +1,13 @@
+package com.CA.DMoraniecCA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DMoraniecCaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DMoraniecCaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CA/DMoraniecCA/DMoraniecCaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CA.DMoraniecCA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DMoraniecCaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
